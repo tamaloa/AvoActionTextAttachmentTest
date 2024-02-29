8167d849074a6e066423a12d72fc1fc278b40db4
Michael Prilop <prilop@infai.org>
2024-02-29 11:18:48 +0100

    rails generate scaffold Note content:rich_text

---
+++ app/avo/resources/note.rb
@@ -0,0 +1,11 @@
+class Avo::Resources::Note < Avo::BaseResource
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :content, as: :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_101051) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_101800) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -49,7 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_101051) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "messages", force: :cascade do |t|
+  create_table "notes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
